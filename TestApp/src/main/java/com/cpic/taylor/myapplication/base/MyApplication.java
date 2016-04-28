9cf05722c693d02910b68b507cce0baaba4cb943
cf0566 <cf0566@qq.com>
2016-04-28 17:44:20 +0800

    大写的服

---
+++ TestApp/src/main/java/com/cpic/taylor/myapplication/base/MyApplication.java
@@ -0,0 +1,12 @@
+package com.cpic.taylor.myapplication.base;
+
+import android.app.Application;
+
+/**
+ * Created by Taylor on 2016/4/28.
+ */
+public class MyApplication extends Application{
+
+
+
+}
