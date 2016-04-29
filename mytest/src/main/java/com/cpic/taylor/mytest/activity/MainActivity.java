ae868ffac8867778fd247843239cb9ce55e1e01b
cf0566 <cf0566@qq.com>
2016-04-29 10:37:43 +0800

    this is a test application

---
+++ mytest/src/main/java/com/cpic/taylor/mytest/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cpic.taylor.mytest.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cpic.taylor.mytest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
