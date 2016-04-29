501c506d67fa29f4a6f2264e93b0001a227da8c0
cf0566 <cf0566@qq.com>
2016-04-29 09:20:58 +0800

    first commit

--- TestApp/src/main/java/com/cpic/taylor/myapplication/activity/MainActivity.java
+++ TestApp/src/main/java/com/cpic/taylor/myapplication/activity/MainActivity.java
@@ -2,14 +2,26 @@ package com.cpic.taylor.myapplication.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.cpic.taylor.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = (Button) findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"点击",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
